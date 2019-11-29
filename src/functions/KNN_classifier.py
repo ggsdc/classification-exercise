1f8b2e11515b3d068e3bedaa00089e74630c6cab
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2019-11-29 18:42:08 +0100

    Added train and test split. Added fixed seed. Added stratified-k fold cross validation

--- src/functions/KNN_classifier.py
+++ src/functions/KNN_classifier.py
@@ -4,27 +4,38 @@ from sklearn.neighbors import KNeighborsClassifier
 # TODO: try with different weights and distances to get different results -> Brute force
 # TODO: brute force number of neighbors.
 
-def knn_classifier(X_train, y_train, X_test, y_test):
+def knn_classifier(X_train, y_train, X_test, y_test, folds):
     weights = ['uniform', 'distance']
     neighbors = [3, 5, 7, 9, 11]
-    distances = [1, 2] # Manhattan and euclidean
+    distances = [1, 2, 3] # Manhattan and euclidean
     best_model = KNeighborsClassifier()
     best_accuracy = 0
+    results = dict()
     for n in neighbors:
         for w in weights:
             for d in distances:
-                model = KNeighborsClassifier(n_neighbors=n, weights=w, algorithm='auto', p=d)
-                model.fit(X_train, y_train)
-                y_predict = model.predict(X_test)
-                y_predict_prob = model.predict(X_test)
-                # TODO: calcuate accuracy and compare with best
-                # Maybe it does not have to be accuracy but it can be any other metric.
-                accuracy = 100
-                if accuracy > best_accuracy:
-                    best_model = model
+                results[(n, w, d)] = list()
+                print(n,w,d)
+                count = 1
+                for idx_train, idx_test in folds.split(X_train, y_train):
+                    print(count)
+                    X_train_folds = X_train[idx_train]
+                    X_test_folds = X_train[idx_test]
+                    y_train_folds = y_train[idx_train]
+                    y_test_folds = y_train[idx_test]
+                    print('TRAIN')
+                    model = KNeighborsClassifier(n_neighbors=n, weights=w, algorithm='kd_tree', p=d, n_jobs=-1)
+                    # print(model)
+                    model.fit(X_train_folds, y_train_folds.reshape(X_train_folds.shape[0],))
+                    print('TEST')
+                    results[(n,w,d)].append(model.score(X_test_folds, y_test_folds))
+                    print('DONE')
+                    count += 1
+
+
 
 
     # TODO: Decide what to return to make it the same for all models
     # Metrics and model maybe? -> With metrics decide best generalized model.
     # Extract best model metrics
-    return True
+    return results
--- src/main.py
+++ src/main.py
@@ -1,10 +1,32 @@
 import pandas as pd
-from .functions.KNN_classifier import knn_classifier
+import random
+from src.functions.KNN_classifier import knn_classifier
+from sklearn.model_selection import StratifiedKFold, train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+random.seed(9)
 
 pd.set_option('display.max_columns', 210)
 
 data = pd.read_csv('./data/train.csv')
-# TODO: data preprocessing.
+data = data.drop(columns=['ID_code'])
+values = data.values
+scaler = MinMaxScaler()
+data_scaled = scaler.fit_transform(values)
+X = data_scaled[:, 1:]
+y = data_scaled[:, 0:1]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+folds = StratifiedKFold(n_splits=10, shuffle=False)
+
+results = knn_classifier(X_train, y_train, X_test, y_test, folds)
+
+
+
+
+
 # TODO: cross-validation?
 # TODO: model looping.
 # TODO: model selection.
