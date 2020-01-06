1624e624a5bf386cddf2531b3e8d23b4157d5eb8
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2020-01-06 23:31:41 +0100

    Even more simplifications

--- src/functions/KNN_classifier.py
+++ src/functions/KNN_classifier.py
@@ -58,50 +58,28 @@ def knn_classifier(x_train, y_train, x_test, y_test, folds, n_attributes):
                 print(model, x_order.shape, y_order.shape, x_store.shape, y_store.shape)
                 loop = False
 
-        results[d] = model.score(x_test, y_test)
-        print(d, results[d])
+        results[d]['acc'] = model.score(x_test, y_test)
+        results[d]['x_order'] = x_order
+        results[d]['y_order'] = y_order
+        results[d]['x_store'] = x_store
+        results[d]['y_store'] = y_store
+
+        print(d, results[d]['acc'])
 
     best_acc = 0
     best_config = ''
     for i in results:
-        total_acc = results[i]
+        total_acc = results[i]['acc']
         if total_acc > best_acc:
             best_acc = total_acc
             best_config = i
 
     print(best_config, best_acc)
-    order_idx = np.delete(np.arange(x_train.shape[0]), 0)
-    x_store = x_train[0]
-    y_store = y_train[0]
-    x_order = x_train[order_idx]
-    y_order = y_train[order_idx]
-    loop = True
-    loop_count = 1
+    x_store = results[best_config]['x_store']
+    y_store = results[best_config]['y_store']
 
     best_model = KNeighborsClassifier(n_neighbors=1, weights='uniform', algorithm='kd_tree', p=best_config)
-    while loop:
-        best_model = KNeighborsClassifier(n_neighbors=1, weights='uniform', algorithm='kd_tree', p=best_config)
-        best_model.fit(x_store.reshape(loop_count, n_attributes), y_store.reshape(loop_count, 1))
-        finished = True
-        for i in range(x_order.shape[0]):
-            predicted = best_model.predict(x_order[i].reshape(1, n_attributes))
-            if predicted != y_order[i]:
-                x_store = np.concatenate(
-                    (x_store.reshape(loop_count, n_attributes), x_order[i].reshape(1, n_attributes)))
-                y_store = np.concatenate((y_store.reshape(loop_count, 1), y_order[i].reshape(1, 1)))
-
-                order_idx = np.delete(order_idx, i)
-
-                x_order = x_train[order_idx]
-                y_order = y_train[order_idx]
-
-                finished = False
-                break
-
-        loop_count += 1
-        if finished:
-            print(best_model, x_order.shape, y_order.shape, x_store.shape, y_store.shape)
-            loop = False
+    best_model.fit(x_store, y_store)
 
     y_prediction = best_model.predict(x_test)
     cm = confusion_matrix(y_test, y_prediction)
@@ -113,5 +91,6 @@ def knn_classifier(x_train, y_train, x_test, y_test, folds, n_attributes):
     final_results['acc'] = best_model.score(x_test, y_test)
     final_results['cm'] = cm
     final_results['cr'] = cr
+    print(final_results['acc'])
 
     return final_results
